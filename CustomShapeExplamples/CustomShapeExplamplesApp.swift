eb65f610f932ae5ca368282ec1734fec1b8b5527
Nathan Smith <nsmith@lcs.on.ca>
2022-11-17 11:45:00 -0500

    Initial Commit

---
+++ CustomShapeExplamples/CustomShapeExplamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapeExplamplesApp.swift
+//  CustomShapeExplamples
+//
+//  Created by Nathan on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapeExplamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
